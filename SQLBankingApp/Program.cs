15d36c99cbf30df80f27a14583732a9bd8d6f2b2
Seth Bowman <brandonsethbowman@gmail.com>
2024-02-28 12:46:55 -0600

    Cleaned up main method by moving base functionality to a method in the ATM class

--- SQLBankingApp/Program.cs
+++ SQLBankingApp/Program.cs
@@ -1,29 +1,3 @@
-﻿using SQLBankingApp.DataAccess;
-using SQLBankingApp.DataManipulation;
-using SQLBankingApp.UserInterface;
+﻿using SQLBankingApp.UserInterface;
 
-bool cont = true;
-
-while (cont)
-{
-
-    IBankAccountRepo bankAccountRepo = new BankAccountRepo(JSONAccess.JSONConfig());
-
-    Console.WriteLine("Please enter your account number:");
-    var accountNumber = Console.ReadLine();
-
-    Console.WriteLine("Please enter your password:");
-    var password = Console.ReadLine();
-
-    var account = bankAccountRepo.GetAllAccounts().FirstOrDefault(a => a.AccountNumber == accountNumber);
-
-    if (account != null && account.VerifyPassword(password))
-    {
-        cont = ATM.ATMInterface(account);
-    }
-    else
-    {
-        Console.WriteLine("\nInvalid account number or password. Please try again.\n\n");
-        continue;
-    }
-}
+ATM.AppInterface();
--- SQLBankingApp/UserInterface/ATM.cs
+++ SQLBankingApp/UserInterface/ATM.cs
@@ -1,4 +1,6 @@
-﻿using SQLBankingApp.Models;
+﻿using SQLBankingApp.DataAccess;
+using SQLBankingApp.DataManipulation;
+using SQLBankingApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -51,6 +53,34 @@ namespace SQLBankingApp.UserInterface
                     default:
                         Console.WriteLine("\nInvalid option. Please enter a valid option.");
                         break;
+                }
+            }
+        }
+
+        public static void AppInterface()
+        {
+            bool cont = true;
+
+            while (cont)
+            {
+                IBankAccountRepo bankAccountRepo = new BankAccountRepo(JSONAccess.JSONConfig());
+
+                Console.WriteLine("Please enter your account number:");
+                var accountNumber = Console.ReadLine();
+
+                Console.WriteLine("Please enter your password:");
+                var password = Console.ReadLine();
+
+                var account = bankAccountRepo.GetAllAccounts().FirstOrDefault(a => a.AccountNumber == accountNumber);
+
+                if (account != null && account.VerifyPassword(password))
+                {
+                    cont = ATM.ATMInterface(account);
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid account number or password. Please try again.\n\n");
+                    continue;
                 }
             }
         }
